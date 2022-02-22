fd644be3eb4fcb67ab28bea28c06f2aa357e4957
Shichang Zhang <77728761+Shichang-Zhang@users.noreply.github.com>
2022-02-22 01:11:32 -0500

    unknown load memory result

--- MiniProject_2/cache.vhd
+++ MiniProject_2/cache.vhd
@@ -55,8 +55,9 @@ architecture arch of cache is
     SIGNAL write_waitreq_reg: STD_LOGIC := '1';
 	SIGNAL read_waitreq_reg: STD_LOGIC := '1';
 
-  	begin
+  begin
   	combine: process(clock,reset)
+    	VARIABLE mem_word_counter : INTEGER RANGE 0 TO 4;
   	begin
     -- Initialization 
     IF(now < 1 ps)THEN
@@ -145,52 +146,78 @@ architecture arch of cache is
                     
           	when Load_Memory_To_Cache =>
             
-            	--memory address = (req_tag * block_num + block_offset)*4*4+word_offset*4 
-            	m_addr <= (to_integer(unsigned(req_tag)) * block_num + req_block_offset)*4*4 + req_word_offset*4+memory_counter;
-                m_read <='1';
-              
-                --need 4 cycles to read the data from memory
-                if memory_counter>1 and memory_counter<4 then
+            	if mem_word_counter =0 then 
+                	--memory address = (req_tag * block_num + block_offset)*4*4+word_offset*4 
+                    m_addr <= (to_integer(unsigned(req_tag)) * block_num + req_block_offset)*4*4 + (req_word_offset)*4+memory_counter;
+                    m_read <='1';
 
-                	current_access_set(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;
-                    memory_counter <=memory_counter+1;
-                elsif memory_counter=4 then 
+                    --need 4 cycles to read the data from memory
+                    if memory_counter>1 and memory_counter<4 then
+                        current_access_set(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;
+                        memory_counter <=memory_counter+1;
+                    elsif memory_counter=4 then 
+                        current_access_set(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;                  
+                        m_addr <= 0;
+                        memory_counter <= memory_counter+1;
+                        m_read <='0';
+                    elsif memory_counter=5 then
+                        m_addr <= 0;
+                        m_read <='0';
+                        current_access_set(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;
+                        --set the valid bit and dirty bit and tag;
+                        current_access_set(39)<='1';
+                        current_access_set(38)<='0';
+                        current_access_set(37 downto 32) <= req_tag;
 
-					current_access_set(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;
-                    
-                    m_addr <= 0;
-                    memory_counter <= memory_counter+1;
-                    m_read <='0';
-                elsif memory_counter=5 then
-                	m_addr <= 0;
-                    m_read <='0';
-
-					current_access_set(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;
-                	--set the valid bit and dirty bit and tag;
-					current_access_set(39)<='1';
-					current_access_set(38)<='0';
-					current_access_set(37 downto 32) <= req_tag;
-
-                    --change the state
-                    if (s_read = '1' AND s_write = '1') then 
-                    	--cannot read and write simultanously
-                  		current_state <= IDLE;
-              		elsif s_read = '1' then
-                  		current_state <= Read_From_Cache;
-              		elsif s_write = '1' then
-                  		current_state <= Write_To_Cache;
-              		else
-                  		current_state <= IDLE;
-              		end if;
-                    memory_counter <=0;
-                else 
-                  	memory_counter <=memory_counter+1;
+                        memory_counter <=0;
+                        mem_word_counter := mem_word_counter+1;
+                    else 
+                        memory_counter <=memory_counter+1;
+                    end if;
+                elsif mem_word_counter>0 and mem_word_counter<4 then                	
+                    m_addr <= (to_integer(unsigned(req_tag)) * block_num + req_block_offset)*4*4 + (req_word_offset+mem_word_counter)*4+memory_counter;
+                    m_read <='1';
+                    --need 4 cycles to read the data from memory
+                    if memory_counter>1 and memory_counter<4 then
+                        cache_storage(4*req_block_offset+req_word_offset+mem_word_counter)(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;
+                        memory_counter <=memory_counter+1;
+                    elsif memory_counter=4 then 
+                        cache_storage(4*req_block_offset+req_word_offset+mem_word_counter)(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;                  
+                        m_addr <= 0;
+                        memory_counter <= memory_counter+1;
+                        m_read <='0';
+                    elsif memory_counter=5 then
+                        m_addr <= 0;
+                        m_read <='0';
+                        cache_storage(4*req_block_offset+req_word_offset+mem_word_counter)(((memory_counter-2+1)*8-1) downto (memory_counter-2)*8) <= m_readdata;
+                        --set the valid bit and dirty bit and tag;
+                        cache_storage(4*req_block_offset+req_word_offset+mem_word_counter)(39)<='1';
+                        cache_storage(4*req_block_offset+req_word_offset+mem_word_counter)(38)<='0';
+                        cache_storage(4*req_block_offset+req_word_offset+mem_word_counter)(37 downto 32) <= req_tag;
+                        memory_counter <=0;
+                        mem_word_counter := mem_word_counter+1;
+                    else 
+                        memory_counter <=memory_counter+1;
+                    end if;
+                end if;
+                
+                if mem_word_counter=4 then
+                	if (s_read = '1' AND s_write = '1') then 
+	                    	--cannot read and write simultanously
+	                  		current_state <= IDLE;
+	              	elsif s_read = '1' then
+	                  		current_state <= Read_From_Cache;
+	              	elsif s_write = '1' then
+	                  		current_state <= Write_To_Cache;
+	              	else
+	                  		current_state <= IDLE;
+	              	end if;
+                    mem_word_counter :=0;
                 end if;
             		
           	when Read_From_Cache =>
             	cache_storage(4*req_block_offset+req_word_offset)<=current_access_set;
             	s_readdata <= cache_storage(4*req_block_offset+req_word_offset)(31 downto 0);
-                report to_string(cache_storage(4*req_block_offset+req_word_offset)(31 downto 0))  severity note;
                 report to_string(s_readdata) severity note;
 --                 s_read <= '0';
                 s_waitrequest <= '0';
