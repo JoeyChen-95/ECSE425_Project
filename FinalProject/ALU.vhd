b3cdf708831c0165a01b6718dfd0214065437053
Shichang-Zhang <77728761+Shichang-Zhang@users.noreply.github.com>
2022-04-06 23:09:46 -0400

    alu good

--- FinalProject/ALU.vhd
+++ FinalProject/ALU.vhd
@@ -1,171 +1,197 @@
+-- Code your design here
 library IEEE;
 use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
 
-entity ALU is 
-  port (
-  	-- clock, reset, stall
-    ALU_clock: in std_logic;
---     ALU_reset: in std_logic;
---     ALU_stall: in std_logic;
-    
-    -- Rs,Rt,operand, output result
-    ALU_RS: in std_logic_vector(31 downto 0);
-    ALU_RT_or_immediate: in std_logic_vector(31 downto 0); -- Rt or the immediate value
-    ALU_operand_code: in std_logic_vector(5 downto 0);
-    ALU_result_out: out std_logic_vector(31 downto 0) 
+entity EX is
+  port(
+    -- clock, reset, stall
+    ex_clock: in std_logic;
+    ex_reset: in std_logic;
+    ex_stall: in std_logic;
+
+    EX_Rs_in  : in  STD_LOGIC_VECTOR (31 downto 0); --Rs
+    EX_Rt_in   : in  STD_LOGIC_VECTOR (31 downto 0); --Rt
+    EX_immediate_value   : in  STD_LOGIC_VECTOR (31 downto 0); --immediate value
+    EX_operand_code: in std_logic_vector(4 downto 0);
+    EX_data_out   : out  STD_LOGIC_VECTOR (31 downto 0); -- result of ALU
+
+    -- forwarding data
+    ex_forward_data: in STD_LOGIC_VECTOR (31 downto 0); --data from EX stage
+    mem_forward_data: in std_logic_vector (31 downto 0); --data from memory stage
+
+    -- pass the data that will be used in later stages forward
+    WB_enable_in: in std_logic; --indicate the write back enable
+    WB_enable_out: out std_logic;
+    store_enable_in: in std_logic; -- indicate the store in mem
+    store_enable_out: out std_logic; 
+    load_enable_in: in std_logic; -- indicate the load in mem
+    load_enable_out: out std_logic;
+    Rd_in	: in STD_LOGIC_VECTOR (4 downto 0); --indicate the Rd
+    Rd_out	: out STD_LOGIC_VECTOR (4 downto 0);
+    byte_in:in std_logic; --what??
+    byte_out:out std_logic;
+
+    -- mux select signal
+    Rs_mux_select0   : in  STD_LOGIC;
+    Rs_mux_select1   : in  STD_LOGIC;
+    Rt_mux_select0   : in  STD_LOGIC;
+    Rt_mux_select0   : in  STD_LOGIC;
+
+    -- other signal
+    mem_data_out:out STD_LOGIC_VECTOR (31 downto 0)
   );
 
-end entity ALU;
+end EX;
 
+architecture ex_architecture of EX is 
+  -- ALU component to calculate 
+  component ALU is
+    port (
+      -- clock, reset, stall
+      ALU_clock: in std_logic;
+      ALU_reset: in std_logic;
+      ALU_stall: in std_logic;
+      -- Rs,Rt,operand, output result
+      ALU_RS: in std_logic_vector(31 downto 0);
+      ALU_RT_or_immediate: in std_logic_vector(31 downto 0); -- Rt or the immediate value
+      ALU_operand_code: in std_logic_vector(5 downto 0);
+      ALU_result_out: out std_logic_vector(31 downto 0) 
+    );
+  end component;
 
-architecture ALU_architecture of ALU is 
+  -- mux component with 4 inputs and 2 select signals, use for choose the signal input to ALU
+  component mux_4 is
+    Port ( 
+      mux_4_select_0 : in  STD_LOGIC; --select signal 0
+      mux_4_select_1 : in  STD_LOGIC; --select signal 1
+      mux_4_input_0   : in  STD_LOGIC_VECTOR (31 downto 0); -- input 0
+      mux_4_input_1   : in  STD_LOGIC_VECTOR (31 downto 0); -- input 1
+      mux_4_input_2   : in  STD_LOGIC_VECTOR (31 downto 0); -- input 2
+      mux_4_input_3   : in  STD_LOGIC_VECTOR (31 downto 0); -- input 3
+      mux_4_output   : out STD_LOGIC_VECTOR (31 downto 0)   -- output
+    );
+  end component;
+  
+  -- ALU component signals
+  ALU_clock: in std_logic;
+  ALU_reset: in std_logic;
+  ALU_stall: in std_logic;
+  ALU_RS: in std_logic_vector(31 downto 0);
+  ALU_RT_or_immediate: in std_logic_vector(31 downto 0);
+  ALU_operand_code: in std_logic_vector(5 downto 0);
+  ALU_result_out: out std_logic_vector(31 downto 0) ;
+  
+  -- mux_4 component signals 
+  -- for Rs mux select
+  mux_4_Rs_select_0 : in  STD_LOGIC; 
+  mux_4_Rs_select_1 : in  STD_LOGIC; 
+  mux_4_Rs_input_0 : in  STD_LOGIC_VECTOR (31 downto 0); 
+  mux_4_Rs_input_1 : in  STD_LOGIC_VECTOR (31 downto 0); 
+  mux_4_Rs_input_2 : in  STD_LOGIC_VECTOR (31 downto 0); 
+  mux_4_Rs_input_3 : in  STD_LOGIC_VECTOR (31 downto 0); 
+  mux_4_Rs_output : out STD_LOGIC_VECTOR (31 downto 0);
+  -- for Rt mux select
+  mux_4_Rt_select_0 : in  STD_LOGIC; 
+  mux_4_Rt_select_1 : in  STD_LOGIC; 
+  mux_4_Rt_input_0 : in  STD_LOGIC_VECTOR (31 downto 0); 
+  mux_4_Rt_input_1 : in  STD_LOGIC_VECTOR (31 downto 0); 
+  mux_4_Rt_input_2 : in  STD_LOGIC_VECTOR (31 downto 0); 
+  mux_4_Rt_input_3 : in  STD_LOGIC_VECTOR (31 downto 0); 
+  mux_4_Rt_output : out STD_LOGIC_VECTOR (31 downto 0);
+  
+  -- temp signals
+  signal mux_4_Rs_select : std_logic_vector(1 downto 0) ;
+  signal mux_4_Rt_select : std_logic_vector(1 downto 0) ;
 
-  -- high and low part for mul and div instruction
-  signal high: std_logic_vector(31 downto 0);
-  signal low: std_logic_vector(31 downto 0);
-  signal long: std_logic_vector(63 downto 0);
+begin
 
-  begin 
-
-   process(ALU_RS,ALU_RT_or_immediate,ALU_operand_code) 
-
-    begin  
-
-      case ALU_operand_code is
-		
-      	when "000000" => --add 
-          --Rd = Rs+Rt
-          ALU_result_out <= std_logic_vector(unsigned(ALU_RS) + unsigned(ALU_RT_or_immediate));
-
-       	when "000001" => --sub 
-          --Rd = Rs-Rt
-          ALU_result_out <= std_logic_vector(unsigned(ALU_RS) - unsigned(ALU_RT_or_immediate));
-          
-      	when "000010" => --addi 
-          --Rd = Rs+immediate
-          ALU_result_out <= std_logic_vector(unsigned(ALU_RS) + unsigned(ALU_RT_or_immediate));
-
-       	when "000011" => --mul
-          -- Rd = Rs * Rt
-          long <= std_logic_vector(to_signed(to_integer (unsigned(ALU_RS)) * to_integer (unsigned(ALU_RT_or_immediate)), 64));
-          high <= long (63 downto 32);
-          low <= long(31 downto 0);
-          ALU_result_out <= std_logic_vector(to_signed(to_integer (unsigned(ALU_RS)) * to_integer (unsigned(ALU_RT_or_immediate)), 32));
-          
-        when "000100" => --div
-        -- Rd = Rs / Rt
-          high <= std_logic_vector(to_unsigned(to_integer (unsigned(ALU_RS)) mod to_integer (unsigned(ALU_RT_or_immediate)), 32));
-          low <= std_logic_vector(to_unsigned(to_integer (unsigned(ALU_RS)) / to_integer (unsigned(ALU_RT_or_immediate)), 32));
-          ALU_result_out <= std_logic_vector(to_unsigned(to_integer (unsigned(ALU_RS)) / to_integer (unsigned(ALU_RT_or_immediate)), 32));
-     	
-        when "000101" => --slt 
-          --Rd = (Rs < Rt) ? 1 : 0
-          if unsigned(ALU_RS) < unsigned(ALU_RT_or_immediate) then
-          	ALU_result_out <= "00000000000000000000000000000001";
-          else
-          	ALU_result_out <= "00000000000000000000000000000000";
-          end if;
-
-       	when "000110" => --slti 
-          --slt Rd = (Rs < immediate) ? 1 : 0
-          if unsigned(ALU_RS) < unsigned(ALU_RT_or_immediate) then
-          	ALU_result_out <= "00000000000000000000000000000001";
-          else
-          	ALU_result_out <= "00000000000000000000000000000000";
-          end if;
+  ALU1: ALU 
+  ---------Port Map of ALU ---------
+  port map(
+      ALU_clock => ALU_clock,
+      ALU_reset => ALU_reset,
+      ALU_stall => ALU_stall,
+      ALU_RS => ALU_RS,
+      ALU_RT_or_immediate => ALU_RT_or_immediate,
+      ALU_operand_code => ALU_operand_code,
+      ALU_result_out => ALU_result_out,
+  );
+  
+  ---------Port Map of Mux_4 ---------
+  Mux_4_Rs : mux_4
+  port map (
+      mux_4_select_0 => mux_4_Rs_select_0,
+      mux_4_select_1 => mux_4_Rs_select_1,
+      mux_4_input_0 => mux_4_Rs_input_0,
+      mux_4_input_1 => mux_4_Rs_input_1,
+      mux_4_input_2 => mux_4_Rs_input_2,
+      mux_4_input_3 => mux_4_Rs_input_3,
+      mux_4_output => mux_4_Rs_output
+  );
+  
+  Mux_4_Rt : mux_4
+  port map (
+      mux_4_select_0 => mux_4_Rt_select_0,
+      mux_4_select_1 => mux_4_Rt_select_1,
+      mux_4_input_0 => mux_4_Rt_input_0,
+      mux_4_input_1 => mux_4_Rt_input_1,
+      mux_4_input_2 => mux_4_Rt_input_2,
+      mux_4_input_3 => mux_4_Rt_input_3,
+      mux_4_output => mux_4_Rt_output
+  );
+  
+  -- mux_Rs and mux_Rt control signals
+  mux_4_Rs_select <= mux_4_Rs_select_1 & mux_4_Rs_select_0;
+  mux_4_Rt_select <= mux_4_Rt_select_1 & mux_4_Rt_select_0;
+  
+  process (ex_clock, ex_reset, ex_stall)
+  
+  begin
+  
+	-- When reset
+  	if reset'event and ex_reset='1' then
+  		-- make all signal "0"
+	
+    -- When stall
+  	elsif ex_stall'event and ex_stall='1' then
+  		-- ??
+    
+    elsif ex_clock'event and ex_clock='1' then
+    	--  run normally
         
-        when "000111" => --and
-          -- Rd = Rs and Rt
-          ALU_result_out<= ALU_RS and ALU_RT_or_immediate;
-
-       	when "001000" => --or
-          -- Rd = Rs or Rt
-          ALU_result_out<= ALU_RS or ALU_RT_or_immediate;
+        -- mux_Rs
+        if(mux_4_Rs_select = "00") then
+          ALU_in1 <= ex_forward_data;
+        elsif(mux_4_Rs_select = "01") then
+          ALU_in1 <= mem_forward_data;
+        elsif(mux_4_Rs_select = "10") then
+          ALU_in1 <= EX_Rs_in;
+        else
+          ALU_in1 <= (others => '0');
+        end if;
           
-      	when "001001" => --nor
-          -- Rd = Rs nor Rt
-          ALU_result_out<= ALU_RS nor ALU_RT_or_immediate;
-
-       	when "001010" => --xor
-          -- Rd = Rs xor Rt
-          ALU_result_out<= ALU_RS xor ALU_RT_or_immediate;
-     	
-        when "001011" =>  --andi
-          -- Rd = Rs and immediate
-          ALU_result_out<= ALU_RS and ALU_RT_or_immediate;
-
-        when "001100" =>  --ori
-          -- Rd = Rs or immediate
-          ALU_result_out<= ALU_RS or ALU_RT_or_immediate;
-
-        when "001101" =>  --xori
-          -- Rd = Rs xor immediate
-          ALU_result_out<= ALU_RS xor ALU_RT_or_immediate;
+        -- mux_Rt
+        if(mux_4_Rt_select = "00") then
+          ALU_in1 <= ex_forward_data;
+        elsif(mux_4_Rt_select = "01") then
+          ALU_in1 <= mem_forward_data;
+        elsif(mux_4_Rt_select = "10") then
+          ALU_in1 <= EX_Rt_in;
+        else
+          ALU_in1 <= EX_immediate_value;
+        end if;
         
-        when "001110" =>  --mfhi
-          -- move the high part to the output
-          ALU_result_out <= high;
+        -- pass the signals that will be used in later stages forward
+        WB_enable_in  =>  WB_enable_out: out std_logic;
+        store_enable_in =>  store_enable_out: out std_logic; 
+        load_enable_in => load_enable_out: out std_logic;
+        Rd_in => Rd_out	: out STD_LOGIC_VECTOR (4 downto 0);
+        byte_in =>  byte_out:out std_logic;
+    
+    end if;
+    
+  end process;
 
-        when "001111" =>  --mflo
-          -- move the low part to the output
-          ALU_result_out <= low;
 
-        when "010000" =>  --lui
-          --  rt<31,16> = immed,  rt<15,0> = 0
-          ALU_result_out <= ALU_RT_or_immediate (15 downto 0)  & "0000000000000000";
-        
-        when "010001" =>  --sll
-          -- shift left by Rt bits, so load 31 - Rt bits of Rs and add Rt bits of 0
-          ALU_result_out <= ALU_RS((31-to_integer(unsigned(ALU_RT_or_immediate))) downto 0) & std_logic_vector(to_unsigned(0,to_integer(unsigned(ALU_RT_or_immediate))));
-
-        when "010010" =>  --srl
-          -- shift right by Rt bits, so Rt bits 0, and add 31 - Rt bits of Rs
-          ALU_result_out <= std_logic_vector(to_unsigned(0,to_integer(unsigned(ALU_RT_or_immediate)))) & ALU_RS( 31 downto (to_integer(unsigned(ALU_RT_or_immediate))));
-        
-        when "010011" =>  --sra
-          -- if the most significant bit of Rs is 1, add 1, otherwise add by zero, and with 31 - Rt bits of Rs
-          if ALU_RS(31) = '0' then
-          	ALU_result_out <= std_logic_vector(to_unsigned(0,to_integer(unsigned(ALU_RT_or_immediate)))) & ALU_RS( 31 downto (to_integer(unsigned(ALU_RT_or_immediate))));
-          else 
-            ALU_result_out <= std_logic_vector(to_unsigned(1,to_integer(unsigned(ALU_RT_or_immediate)))) & ALU_RS( 31 downto (to_integer(unsigned(ALU_RT_or_immediate))));
-          end if;
-          
-        when "010100" =>  --lw
-          -- since we use a control logic to determine whether we should load, so only calculate address
-          ALU_result_out<= std_logic_vector(unsigned(ALU_RS) + unsigned(ALU_RT_or_immediate));
-       
-        when "010101" =>  --sw
-          -- since we use a control logic to determine whether we should store, so only calculate address
-          ALU_result_out<= std_logic_vector(unsigned(ALU_RS) + unsigned(ALU_RT_or_immediate));
-        
-        when "010110" =>  --beq
-          -- since use a comparator, so only calculate the address : addr = pc + immediate
-          ALU_result_out<= std_logic_vector(unsigned(ALU_RS) + unsigned(ALU_RT_or_immediate));
-       
-        when "010111" =>  --bne
-          -- since use a comparator, so only calculate the address : addr = pc + immediate
-          ALU_result_out<= std_logic_vector(unsigned(ALU_RS) + unsigned(ALU_RT_or_immediate));
-          
-        when "011000" =>  --j
-          -- jump, address = Rs
-          ALU_result_out<= ALU_RS;  
-       
-        when "011001" =>  --jr
-          -- jump register, address = Rs
-          ALU_result_out<= ALU_RS;
-        
-        when "011010" =>  --jal
-          -- jump and link, address = Rs
-          ALU_result_out<= ALU_RS;
-          
-        when others =>
-          -- undefined instruction
-		  ALU_result_out <= "00000000000000000000000000000000";
-
-      end case;
-
-    end process; 
-
-end architecture ; 
+end architecture;
 
--- FinalProject/unit_tests/ALU_tb.vhd
+++ FinalProject/unit_tests/ALU_tb.vhd
@@ -2,44 +2,58 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
-ENTITY ALU_tb is
-end ALU_tb;
+ENTITY EX_tb is
+end EX_tb;
 
-ARCHITECTURE ALU_testbench OF ALU_tb is
+architecture EX_testbench OF EX_tb is
 
-	COMPONENT ALU is
-    port (
-      ALU_clock: in std_logic;
---       ALU_reset: in std_logic;
---       ALU_stall: in std_logic;
-      ALU_RS: in std_logic_vector(31 downto 0);
-      ALU_RT_or_immediate: in std_logic_vector(31 downto 0);
-      ALU_operand_code: in std_logic_vector(5 downto 0);
-      ALU_result_out: out std_logic_vector(31 downto 0) 
-  	);    
+	COMPONENT EX is
+    port(
+    -- clock, reset, stall
+    ex_clock: in std_logic;
+    ex_reset: in std_logic;
+    ex_stall: in std_logic;
+
+    EX_Rs_in  : in  STD_LOGIC_VECTOR (31 downto 0); --Rs
+    EX_Rt_in   : in  STD_LOGIC_VECTOR (31 downto 0); --Rt
+    EX_immediate_value   : in  STD_LOGIC_VECTOR (31 downto 0); --immediate value
+    EX_operand_code: in std_logic_vector(4 downto 0);
+    EX_data_out   : out  STD_LOGIC_VECTOR (31 downto 0); -- result of ALU
+
+    -- forwarding data
+    ex_forward_data: in STD_LOGIC_VECTOR (31 downto 0); --data from EX stage
+    mem_forward_data: in std_logic_vector (31 downto 0); --data from memory stage
+
+    -- pass the data that will be used in later stages forward
+    WB_enable_in: in std_logic; --indicate the write back enable
+    WB_enable_out: out std_logic;
+    store_enable_in: in std_logic; -- indicate the store in mem
+    store_enable_out: out std_logic; 
+    load_enable_in: in std_logic; -- indicate the load in mem
+    load_enable_out: out std_logic;
+    Rd_in	: in STD_LOGIC_VECTOR (4 downto 0); --indicate the Rd
+    Rd_out	: out STD_LOGIC_VECTOR (4 downto 0);
+    byte_in:in std_logic; --what??
+    byte_out:out std_logic;
+
+    -- mux select signal
+    Rs_mux_select0   : in  STD_LOGIC;
+    Rs_mux_select1   : in  STD_LOGIC;
+    Rt_mux_select0   : in  STD_LOGIC;
+    Rt_mux_select0   : in  STD_LOGIC;
+
+    -- other signal
+    mem_data_out:out STD_LOGIC_VECTOR (31 downto 0)
+    );    
     END COMPONENT;
     
     -- test signals
     signal clk : std_logic := '0';
     constant clk_period : time := 1 ns;
-	signal ALU_RS: std_logic_vector(31 downto 0);
-    signal ALU_RT_or_immediate: std_logic_vector(31 downto 0);
-    signal ALU_result_out: std_logic_vector(31 downto 0);
-    signal ALU_operand_code : std_logic_vector(5 downto 0);
+
      
 begin 
-	
-  ALU_test: ALU 
-  ---------Port Map of ALU ---------
-  port map(
-      ALU_clock => clk,
-      ALU_RS => ALU_RS,
-      ALU_RT_or_immediate => ALU_RT_or_immediate,
-      ALU_result_out => ALU_result_out,
-      ALU_operand_code => ALU_operand_code
-  );
-  
-  
+
   clk_process : process
   begin
     clk <= '0';
@@ -48,278 +62,9 @@ begin
     wait for clk_period/2;
   end process;
   
-  
   test_process : process
   begin
-	
-    -- test add 2ns
-  	wait for clk_period;
-    ALU_RS <= x"00000001";
-    ALU_RT_or_immediate <= x"00000002";
-    ALU_operand_code <= "000000";
-    wait for clk_period;
-    report to_string(ALU_result_out) severity note;
-    
-    -- test sub 4ns
-  	wait for clk_period;
-    ALU_RS <= x"00000003";
-    ALU_RT_or_immediate <= x"00000002";
-    ALU_operand_code <= "000001";
-    wait for clk_period;
-    report to_string(ALU_result_out) severity note;
-    
-    -- test addi 6ns
-  	wait for clk_period;
-    ALU_RS <= x"00000001";
-    ALU_RT_or_immediate <= x"00001111";
-    ALU_operand_code <= "000010";
-    wait for clk_period;
-    report to_string(ALU_result_out) severity note;    
-    
-    -- test mul 8ns
-  	wait for clk_period;
-    ALU_RS <= x"00000123"; --291
-    ALU_RT_or_immediate <= x"00000234"; --564
-    ALU_operand_code <= "000011";
-    wait for clk_period;
-    -- should be 164124, 101000000100011100
-    report to_string(ALU_result_out) severity note;   
-    
-    -- test div 10ns
-  	wait for clk_period;
-    ALU_RS <= x"0002811c";--164124
-    ALU_RT_or_immediate <= x"00000123"; --291
-    ALU_operand_code <= "000100";
-    wait for clk_period;
-    -- should be 564, 1000110100
-    report to_string(ALU_result_out) severity note;
-    
-    -- test slt 1 Rs < Rt 12ns
-  	wait for clk_period;
-    ALU_RS <= x"00000001";
-    ALU_RT_or_immediate <= x"00001111";
-    ALU_operand_code <= "000101";
-    wait for clk_period;
-    --expect 1
-    report to_string(ALU_result_out) severity note; 
-    
-    -- test slt 1 Rs > Rt 14ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "000101";
-    wait for clk_period;
-    -- expect 0
-    report to_string(ALU_result_out) severity note; 
-    
-    -- test slti 1 Rs < Rt 16ns
-  	wait for clk_period;
-    ALU_RS <= x"00000001";
-    ALU_RT_or_immediate <= x"00001111";
-    ALU_operand_code <= "000110";
-    wait for clk_period;
-    --expect 1
-    report to_string(ALU_result_out) severity note; 
-    
-    -- test slti 1 Rs > Rt 18ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "000110";
-    wait for clk_period;
-    -- expect 0
-    report to_string(ALU_result_out) severity note; 
-    
-    -- test and 20ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "000111";
-    wait for clk_period;
-    -- expect 0x00000001
-    report to_string(ALU_result_out) severity note; 
-    
-    -- test or 22ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "001000";
-    wait for clk_period;
-    -- expect 0x00001111
-    report to_string(ALU_result_out) severity note; 
-    
-    -- test nor, 24ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "001001";
-    wait for clk_period;
-    -- expect 11111111111111111110111011101110
-    report to_string(ALU_result_out) severity note;
-    
-    -- test xor, 26ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "001010";
-    wait for clk_period;
-    -- expect 0x00001110
-    report to_string(ALU_result_out) severity note;
-    
-    -- test andi 28ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "001011";
-    wait for clk_period;
-    -- expect 0x00000001
-    report to_string(ALU_result_out) severity note; 
-    
-    -- test ori 30ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "001100";
-    wait for clk_period;
-    -- expect 0x00001111
-    report to_string(ALU_result_out) severity note;   
-    
-    -- test xori 32ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "001101";
-    wait for clk_period;
-    -- expect 0x00001110
-    report to_string(ALU_result_out) severity note;  
-    
-    -- test mfhi 34ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "001110";
-    wait for clk_period;
-    -- expect ??
-    report to_string(ALU_result_out) severity note;
-
-    -- test mflo 36ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000001";
-    ALU_operand_code <= "001111";
-    wait for clk_period;
-    -- expect ??
-    report to_string(ALU_result_out) severity note;
-    
-    -- test lui 38ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00001f12";
-    ALU_operand_code <= "010000";
-    wait for clk_period;
-    -- expect 00011111000100100000000000000000
-    report to_string(ALU_result_out) severity note;    
-
-    -- test sll 40ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "010001";
-    wait for clk_period;
-    -- expect 0x00008888, 00000000000000001000100010001000
-    report to_string(ALU_result_out) severity note;
-	
-    -- test srl 42ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "010010";
-    wait for clk_period;
-    -- expect 0x00000222, 00000000000000000000001000100010
-    report to_string(ALU_result_out) severity note;
-
-    -- test sra, start with 0,  44ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "010011";
-    wait for clk_period;
-    -- expect 0x00000222, 00000000000000000000001000100010
-    report to_string(ALU_result_out) severity note;
-    
-    -- test sra, start with 1,  46ns
-  	wait for clk_period;
-    ALU_RS <= x"90001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "010011";
-    wait for clk_period;
-    -- expect 0xe0000222, 11100000000000000000001000100010 
-    report to_string(ALU_result_out) severity note;
-    
-    -- test lw, 48ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "010100";
-    wait for clk_period;
-    -- expect 0x00001114,  
-    report to_string(ALU_result_out) severity note;   
-    
-    -- test sw, 50ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "010101";
-    wait for clk_period;
-    -- expect 0x00001114
-    report to_string(ALU_result_out) severity note;
-    
-    -- test beq, 52ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "010110";
-    wait for clk_period;
-    -- expect 0x00001114
-    report to_string(ALU_result_out) severity note;
-
-    -- test bne, 54ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "010111";
-    wait for clk_period;
-    -- expect 0x00001114
-    report to_string(ALU_result_out) severity note;
-    
-    -- test j, 56ns
-  	wait for clk_period;
-    ALU_RS <= x"00001111";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "011000";
-    wait for clk_period;
-    -- expect 0x00001111
-    report to_string(ALU_result_out) severity note;
-    
-    -- test jr, 58ns
-  	wait for clk_period;
-    ALU_RS <= x"00001112";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "011001";
-    wait for clk_period;
-    -- expect 0x00001112
-    report to_string(ALU_result_out) severity note;    
-
-    -- test jal, 60ns
-  	wait for clk_period;
-    ALU_RS <= x"00001113";
-    ALU_RT_or_immediate <= x"00000003";
-    ALU_operand_code <= "011010";
-    wait for clk_period;
-    -- expect 0x00001113
-    report to_string(ALU_result_out) severity note;
-    
-
+  
   end process;
 
-end architecture ;
+end architecture;
