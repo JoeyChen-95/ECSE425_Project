43f35a4d29257224333c9ad9b7ad9909577cc048
IngTian <tzy20001018@gmail.com>
2022-04-09 16:51:25 -0400

    fix: fixed data memory

--- FinalProject/Data_Memory.vhd
+++ FinalProject/Data_Memory.vhd
@@ -1,70 +1,62 @@
 --Adapted from Example 12-15 of Quartus Design and Synthesis handbook
 LIBRARY ieee;
-USE ieee.std_logic_1164.all;
-USE ieee.numeric_std.all;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
 
-ENTITY memory IS
-	GENERIC(
-		ram_size : INTEGER := 32768;
-		mem_delay : time := 10 ns;
-		clock_period : time := 1 ns
+ENTITY data_memory IS
+	GENERIC (
+		RAM_SIZE : INTEGER := 8192;
+		CLOCK_PERIOD : TIME := 1 ns
 	);
 	PORT (
-		clock: IN STD_LOGIC;
-		writedata: IN STD_LOGIC_VECTOR (7 DOWNTO 0);
-		address: IN INTEGER RANGE 0 TO ram_size-1;
-		memwrite: IN STD_LOGIC;
-		memread: IN STD_LOGIC;
-		readdata: OUT STD_LOGIC_VECTOR (7 DOWNTO 0);
-		waitrequest: OUT STD_LOGIC
+		clock : IN STD_LOGIC;
+		reset : IN STD_LOGIC;
+		writedata : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+		address : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+		memwrite : IN STD_LOGIC;
+		memread : IN STD_LOGIC;
+		readdata : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+		waitrequest : OUT STD_LOGIC
 	);
-END memory;
+END data_memory;
 
-ARCHITECTURE rtl OF memory IS
-	TYPE MEM IS ARRAY(ram_size-1 downto 0) OF STD_LOGIC_VECTOR(7 DOWNTO 0);
-	SIGNAL ram_block: MEM;
-	SIGNAL read_address_reg: INTEGER RANGE 0 to ram_size-1;
-	SIGNAL write_waitreq_reg: STD_LOGIC := '1';
-	SIGNAL read_waitreq_reg: STD_LOGIC := '1';
+ARCHITECTURE rtl OF data_memory IS
+	TYPE MEM IS ARRAY(RAM_SIZE - 1 DOWNTO 0) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
+	SIGNAL ram_block : MEM;
+	VARIABLE write_waitreq_reg : STD_LOGIC := '1';
+	VARIABLE read_waitreq_reg : STD_LOGIC := '1';
 BEGIN
-	--This is the main section of the SRAM model
-	mem_process: PROCESS (clock)
+	-- This is the main section of the SRAM model
+	reset_process : PROCESS (reset)
 	BEGIN
-		--This is a cheap trick to initialize the SRAM in simulation
-		IF(now < 1 ps)THEN
-			For i in 0 to ram_size-1 LOOP
-				ram_block(i) <= std_logic_vector(to_unsigned(i mod 256,8));
+		IF (rising_edge(reset)) THEN
+			-- We shall reset the entire data memory
+			-- to its initial value, id est, 0.
+			FOR i IN 0 TO RAM_SIZE - 1 LOOP
+				ram_block(i) <= (OTHERS => '0');
 			END LOOP;
-		end if;
-
-		--This is the actual synthesizable SRAM block
-		IF (clock'event AND clock = '1') THEN
-			IF (memwrite = '1') THEN
-				ram_block(address) <= writedata;
-			END IF;
-		read_address_reg <= address;
 		END IF;
+
 	END PROCESS;
-	readdata <= ram_block(read_address_reg);
 
+	readdata <= ram_block(to_integer(unsigned(address)));
 
-	--The waitrequest signal is used to vary response time in simulation
-	--Read and write should never happen at the same time.
-	waitreq_w_proc: PROCESS (memwrite)
+	-- The waitrequest signal is used to vary response time in simulation
+	-- Read and write should never happen at the same time.
+	waitreq_w_proc : PROCESS (memwrite)
 	BEGIN
-		IF(memwrite'event AND memwrite = '1')THEN
-			write_waitreq_reg <= '0' after mem_delay, '1' after mem_delay + clock_period;
-
+		IF (rising_edge(memwrite)) THEN
+			ram_block(to_integer(unsigned(address))) <= write_data;
+			write_waitreq_reg <= '0', '1' AFTER CLOCK_PERIOD;
 		END IF;
 	END PROCESS;
 
-	waitreq_r_proc: PROCESS (memread)
+	waitreq_r_proc : PROCESS (memread)
 	BEGIN
-		IF(memread'event AND memread = '1')THEN
-			read_waitreq_reg <= '0' after mem_delay, '1' after mem_delay + clock_period;
+		IF (rising_edge(memread)) THEN
+			read_waitreq_reg <= '0' AFTER mem_delay, '1' AFTER mem_delay + CLOCK_PERIOD;
 		END IF;
 	END PROCESS;
-	waitrequest <= write_waitreq_reg and read_waitreq_reg;
-
-
+	
+	waitrequest <= write_waitreq_reg AND read_waitreq_reg;
 END rtl;
