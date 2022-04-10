e684c61f1569bf7d5a7f647d437829e376932e7f
Junjian Chen <77649736+JoeyChen-95@users.noreply.github.com>
2022-04-09 21:13:44 -0400

    Update Data_Memory.vhd

--- FinalProject/Pipelined Processor/Data_Memory.vhd
+++ FinalProject/Pipelined Processor/Data_Memory.vhd
@@ -5,58 +5,44 @@ USE ieee.numeric_std.ALL;
 
 ENTITY data_memory IS
 	GENERIC (
-		RAM_SIZE : INTEGER := 8192;
-		CLOCK_PERIOD : TIME := 1 ns
+		RAM_SIZE : INTEGER := 22;
+		CLOCK_PERIOD : TIME := 1 ns;
 	);
 	PORT (
 		clock : IN STD_LOGIC;
 		reset : IN STD_LOGIC;
 		writedata : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-		address : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+		address: IN INTEGER RANGE 0 TO RAM_SIZE-1;
 		memwrite : IN STD_LOGIC;
 		memread : IN STD_LOGIC;
 		readdata : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-		waitrequest : OUT STD_LOGIC
 	);
 END data_memory;
 
 ARCHITECTURE rtl OF data_memory IS
 	TYPE MEM IS ARRAY(RAM_SIZE - 1 DOWNTO 0) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL ram_block : MEM;
-	VARIABLE write_waitreq_reg : STD_LOGIC := '1';
-	VARIABLE read_waitreq_reg : STD_LOGIC := '1';
+    SIGNAL read_address_reg: INTEGER RANGE 0 TO RAM_SIZE-1;
 BEGIN
 	-- This is the main section of the SRAM model
-	reset_process : PROCESS (reset)
+	mem_process : PROCESS (reset, clock)
 	BEGIN
 		IF (rising_edge(reset)) THEN
 			-- We shall reset the entire data memory
 			-- to its initial value, id est, 0.
 			FOR i IN 0 TO RAM_SIZE - 1 LOOP
-				ram_block(i) <= (OTHERS => '0');
+				ram_block(i) <= X"00000000";
 			END LOOP;
 		END IF;
-
+        
+        if(clock'event and clock='1') then
+        	if(memwrite='1') then
+            	ram_block(address)<=writedata;
+             end if;
+             if(memread='1') then
+             	readdata <= ram_block(address);
+             end if;
+         end if;
 	END PROCESS;
-
-	readdata <= ram_block(to_integer(unsigned(address)));
-
-	-- The waitrequest signal is used to vary response time in simulation
-	-- Read and write should never happen at the same time.
-	waitreq_w_proc : PROCESS (memwrite)
-	BEGIN
-		IF (rising_edge(memwrite)) THEN
-			ram_block(to_integer(unsigned(address))) <= write_data;
-			write_waitreq_reg <= '0', '1' AFTER CLOCK_PERIOD;
-		END IF;
-	END PROCESS;
-
-	waitreq_r_proc : PROCESS (memread)
-	BEGIN
-		IF (rising_edge(memread)) THEN
-			read_waitreq_reg <= '0' AFTER mem_delay, '1' AFTER mem_delay + CLOCK_PERIOD;
-		END IF;
-	END PROCESS;
-	
-	waitrequest <= write_waitreq_reg AND read_waitreq_reg;
+--     readdata <= ram_block(address);
 END rtl;
