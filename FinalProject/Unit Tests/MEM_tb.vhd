ed889c1cdd90e9ff9927d80f244df510895978c7
Junjian Chen <77649736+JoeyChen-95@users.noreply.github.com>
2022-04-09 23:30:11 -0400

    Update MEM_tb.vhd

--- FinalProject/Unit Tests/MEM_tb.vhd
+++ FinalProject/Unit Tests/MEM_tb.vhd
@@ -22,9 +22,9 @@ architecture behaviour of MEM_tb is
   
   				in_address: in std_logic_vector(31 downto 0); -- connect ex_ALU_result_out
   
-  				access_memory_write: in std_logic; -- connect register out
+  				access_memory_write: in std_logic :='0'; -- connect register out
   
-  				access_memory_load: in std_logic; -- connect storeen out
+  				access_memory_load: in std_logic := '1'; -- connect storeen out
 
  				access_reg_address_add_in: in std_logic_vector(4 downto 0); -- connect with ex)dest_regadd_out 
   
@@ -37,8 +37,7 @@ architecture behaviour of MEM_tb is
   				out_data: out std_logic_vector(31 downto 0):= (others=> 'Z');
   				access_reg_out: out std_logic;
   				access_reg_add_out: out std_logic_vector (4 downto 0);
-  				out_waitrequest: out std_logic;
-       	 		);
+		);
 	end component;
     
     --input
@@ -48,7 +47,7 @@ architecture behaviour of MEM_tb is
     signal data_in_forward: std_logic_vector(31 downto 0);
     signal forward_select: std_logic;
     signal in_data: std_logic_vector(31 downto 0);
-    signal in_address: std_logic_vector(31 downto 0);
+    signal in_address: std_logic_vector(31 downto 0) :=X"00000000";
     signal access_memory_write: std_logic;
     signal access_memory_load: std_logic;
     signal access_reg_address_add_in: std_logic_vector(4 downto 0);
@@ -57,7 +56,6 @@ architecture behaviour of MEM_tb is
     signal out_data: std_logic_vector(31 downto 0):= (others=> 'Z');
     signal access_reg_out: std_logic;
     signal access_reg_add_out: std_logic_vector (4 downto 0);
-    signal out_waitrequest: std_logic;
     
 begin
 dut: MEM
@@ -69,13 +67,12 @@ port map(
     in_data=>in_data,
     in_address=>in_address,
     access_memory_write=>access_memory_write,
-    access_memory_load=>access_memory_load,
+    access_memory_load=>access_memory_load,  
     access_reg_address_add_in=>access_reg_address_add_in,
     access_reg_address_in=>access_reg_address_in,
     out_data=>out_data,
     access_reg_out=>access_reg_out,
-    access_reg_add_out=>access_reg_add_out,
-    out_waitrequest=>out_waitrequest
+    access_reg_add_out=>access_reg_add_out
 );
 
 ---------Clock Setup---------
@@ -90,26 +87,31 @@ end process;
 test_process: process
 begin
 	wait for clk_period;
+    reset<='0';
+    wait for clk_period;
+    reset<='1';
+    wait for clk_period;
+    reset<='0';
+    wait for 10*clk_period;
 	access_reg_address_add_in<="11111";
 	access_reg_address_in<='1';
     wait for clk_period;
     assert access_reg_add_out = "11111" report "access_reg_add_out error!!!" severity error;
     assert access_reg_out = '1' report "access_reg_out error!!!" severity error;
-    
-    wait for clk_period;
-    access_memory_load<='1';
-    in_address<=x"00000100";
-    forward_select<='1';
-    data_in_forward<=x"0000000a";
-    in_data<=x"0000000b";
-  
-    wait for clk_period;
-	access_memory_load<='0';
+    wait for 10*clk_period;
     access_memory_write<='1';
-    
+    access_memory_load<='0';
+    wait for 10*clk_period;
+    in_address<=x"00000ddc";
+    in_data<=x"eeeeeeee";
+    data_in_forward<=x"0000ddcc";
     wait for clk_period;
---     access_memory_load<='1';
---     memory_write_data<=x"19181716";
-	wait for clk_period;
+    access_memory_write<='0';
+    access_memory_load<='1';
+    wait for 3*clk_period;
+    access_memory_write<='0';
+    access_memory_load<='1';
+    in_address<=x"000000a0";
+    wait;
 end process;
 end;
