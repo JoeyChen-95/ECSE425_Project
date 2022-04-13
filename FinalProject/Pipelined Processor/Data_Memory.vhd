e9af7c32d0dc9d2c12229cf2a89f0b63ce38d360
Byron Chen <57932129+ByronChen12138@users.noreply.github.com>
2022-04-13 04:57:33 -0400

    Data_Memory.vhd BUG_FREE

--- FinalProject/Pipelined Processor/Data_Memory.vhd
+++ FinalProject/Pipelined Processor/Data_Memory.vhd
@@ -1,4 +1,3 @@
---Adapted from Example 12-15 of Quartus Design and Synthesis handbook
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
@@ -8,8 +7,9 @@ ENTITY Data_Memory IS
 	GENERIC (
 		RAM_SIZE : INTEGER := 8192;
 		CLOCK_PERIOD : TIME := 1 ns;
-        	REGISTER_FILE_ADDRESS : STRING := "memory.txt";
+        REGISTER_FILE_ADDRESS : STRING := "memory.txt";
 	);
+    
 	PORT (
 		dump : IN STD_LOGIC;
 		clock : IN STD_LOGIC;
@@ -17,8 +17,7 @@ ENTITY Data_Memory IS
 		writedata : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
 		address : IN INTEGER RANGE 0 TO RAM_SIZE - 1;
 		memwrite : IN STD_LOGIC;
-		memread : IN STD_LOGIC;
-		readdata : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+		readdata : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
 	);
 END Data_Memory;
 
@@ -26,19 +25,20 @@ ARCHITECTURE rtl OF Data_Memory IS
 	TYPE MEM IS ARRAY(RAM_SIZE - 1 DOWNTO 0) OF STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL ram_block : MEM;
 	SIGNAL read_address_reg : INTEGER RANGE 0 TO RAM_SIZE - 1;
-BEGIN
+    
+BEGIN    
 	-- This is the main section of the SRAM model
 	mem_process : PROCESS (reset, clock, dump)
-    FILE file_ptr : text;
-	VARIABLE file_line : line;
-	VARIABLE line_content : STRING(1 TO 32);
-	BEGIN
-
+    	FILE file_ptr : text;
+		VARIABLE file_line : line;
+		VARIABLE line_content : STD_LOGIC_VECTOR (31 DOWNTO 0);
+    
+    BEGIN
 		IF (rising_edge(reset)) THEN
 			-- We shall reset the entire data memory
 			-- to its initial value, id est, 0.
 			FOR i IN 0 TO RAM_SIZE - 1 LOOP
-				ram_block(i) <= X"00000000";
+				ram_block(i) <= x"00000000";
 			END LOOP;
 		END IF;
 
@@ -49,13 +49,17 @@ BEGIN
 		END IF;
 
 		IF (rising_edge(dump)) THEN
-			file_open(file_ptr, REGISTER_FILE_ADDRESS, READ_MODE);
-			FOR i IN 0 TO 8191 LOOP
-				line_content := to_string(ram_block(i));
-				write(file_line, line_content);
+			file_open(file_ptr, REGISTER_FILE_ADDRESS, write_mode);
+            
+			FOR i IN 0 TO RAM_SIZE - 1 LOOP
+				line_content := ram_block(i);
+				write(file_line, line_content, right, 32);
 				writeline(file_ptr, file_line);
 			END LOOP;
+            
+            file_close(file_ptr);
         end if;
 	END PROCESS;
-		    readdata <= ram_block(address);
+    
+	readdata <= ram_block(address);
 END rtl;
