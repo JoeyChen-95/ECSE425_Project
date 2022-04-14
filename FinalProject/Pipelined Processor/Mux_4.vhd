28a467de89cae3bea6a6a413f7bf78a4cfeb2034
IngTian <tzy20001018@gmail.com>
2022-04-13 23:43:24 -0400

    style: formatted mux

--- FinalProject/Pipelined Processor/Mux_4.vhd
+++ FinalProject/Pipelined Processor/Mux_4.vhd
@@ -1,16 +1,16 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 
--- a mux that has 4 inputs and 2 control signals
+-- A mux that has 4 inputs and 2 control signals
 ENTITY mux_4 IS
   PORT (
-    mux_4_select_0 : IN STD_LOGIC; --select signal 0
-    mux_4_select_1 : IN STD_LOGIC; --select signal 1
-    mux_4_input_0 : IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- input 0
-    mux_4_input_1 : IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- input 1
-    mux_4_input_2 : IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- input 2
-    mux_4_input_3 : IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- input 3
-    mux_4_output : OUT STD_LOGIC_VECTOR (31 DOWNTO 0) -- output
+    mux_4_select_0 : IN STD_LOGIC; -- Select signal 1
+    mux_4_select_1 : IN STD_LOGIC; -- Select signal 1
+    mux_4_input_0 : IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- Input 0
+    mux_4_input_1 : IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- Input 1
+    mux_4_input_2 : IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- Input 2
+    mux_4_input_3 : IN STD_LOGIC_VECTOR (31 DOWNTO 0); -- Input 3
+    mux_4_output : OUT STD_LOGIC_VECTOR (31 DOWNTO 0) -- Output
   );
 END mux_4;
 
