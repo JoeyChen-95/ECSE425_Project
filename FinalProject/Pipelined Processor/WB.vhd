66a4cf9ad41194981b98d6e3fb18ee8ca9bbd219
Junjian Chen <77649736+JoeyChen-95@users.noreply.github.com>
2022-04-14 22:29:34 -0400

    Update WB.vhd

--- FinalProject/Pipelined Processor/WB.vhd
+++ FinalProject/Pipelined Processor/WB.vhd
@@ -1,7 +1,6 @@
 LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
-USE work.memory_arbiter_lib.ALL;
 
 ENTITY WB IS
   PORT (
@@ -16,7 +15,7 @@ ENTITY WB IS
     WB_enable_out : OUT STD_LOGIC;
     WB_address_out : OUT STD_LOGIC_VECTOR (4 DOWNTO 0); -- This 31 should be modified to reg_adrsize
     WB_data_out : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-    WB_forwarding_data : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+    WB_forwarding_data : OUT STD_LOGIC_VECTOR (31 DOWNTO 0)
   );
 END ENTITY;
 
