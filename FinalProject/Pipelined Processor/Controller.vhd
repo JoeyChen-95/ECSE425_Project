8f0fb49321d794c219ef5cc57dbdf53b2a04cd57
Ing Tian <52404420+IngTian@users.noreply.github.com>
2022-04-14 22:28:11 -0400

    no branch prediction

--- FinalProject/Pipelined Processor/Controller.vhd
+++ FinalProject/Pipelined Processor/Controller.vhd
@@ -238,13 +238,180 @@ BEGIN
         VARIABLE id_reg1_internal : unsigned(5 DOWNTO 0);
         VARIABLE id_reg2_internal : unsigned(5 DOWNTO 0);
     BEGIN
+        IF (rising_edge(reset)) THEN
+                -- set all input signal to 0
+                pc_in_pc <= (OTHERS => '0');
+                id_in_instruction <= (OTHERS => '0');
+                id_in_pc<= (OTHERS => '0');
+                id_in_wb_write_enable<= (OTHERS => '0');
+                id_in_wb_write_address<= (OTHERS => '0');
+                id_in_wb_data<= (OTHERS => '0');
+                ex_in_imm<= (OTHERS => '0');
+                ex_in_wb_enable<= (OTHERS => '0');
+                ex_in_store_enable<= (OTHERS => '0');
+                ex_in_load_enable<= (OTHERS => '0');
+                ex_in_imm_enable<= (OTHERS => '0');
+                ex_in_rd<= (OTHERS => '0');
+                ex_in_ex_forward<= (OTHERS => '0');
+                ex_in_mem_forward<= (OTHERS => '0');
+                mem_in_data<= (OTHERS => '0');
+                mem_in_address<= (OTHERS => '0');
+                mem_in_write_enable<= (OTHERS => '0');
+                mem_in_load_enable<= (OTHERS => '0');
+                mem_in_rd<= (OTHERS => '0');
+                mem_in_wb_enable<= (OTHERS => '0');
+                wb_in_wb_enable<= (OTHERS => '0');
+                wb_in_wb_rd<= (OTHERS => '0');
+                wb_in_wb_data<= (OTHERS => '0');
 
+                -- stall other process, add $0, $0, $0
+                ex_in_rs <= (OTHERS => '0');
+                ex_in_rt <= (OTHERS => '0');
+                ex_control_rs_select_forwarding <= '0';
+                ex_control_rs_select_forwarding_mem <='0';
+                ex_control_rt_select_forwarding <= '0';
+                ex_control_rt_select_forwarding_mem <= '0';
+                ex_in_opcode <= "000000";
+
+            
+        end if;
+        
         IF (falling_edge(clk)) THEN
             -- The controller only works at
             -- the falling edge of the clock.
-            
+        
+            id_reg1_internal := id_out_rs_idx ;
+            id_reg2_internal := id_out_rt_idx ;
+
+            -- hazard detection
+            -- only one is Read after write
+            if (wb_out_rd = id_reg1_internal or wb_out_rd = id_reg2_internal) then   
+
+                -- stall other process, add $0, $0, $0
+                ex_in_rs <= (OTHERS => '0');
+                ex_in_rt <= (OTHERS => '0');
+                ex_control_rs_select_forwarding <= '0';
+                ex_control_rs_select_forwarding_mem <='0';
+                ex_control_rt_select_forwarding <= '0';
+                ex_control_rt_select_forwarding_mem <= '0';
+                ex_in_opcode <= "000000";
+
+            elsif (id_out_branch_taken = '1') then
+                -- branch taken, jump to the calculated branch address
+                pc_in_pc  <= id_out_branch_address;            
+                id_in_instruction<= (OTHERS => '0');
+
+
+            else
+                -- pass signals to next stage
+                --IF TO IF
+                pc_in_pc  <= pc_out_pc;
+
+                -- IF to ID
+                id_in_instruction<= pc_out_instruction;
+                id_in_pc  <= pc_out_pc;
+                
+                -- ID to EX
+                ex_in_rs <= id_out_rs;
+                ex_in_rt <= id_out_rt;
+                ex_in_imm <= id_out_imm;
+                ex_in_opcode <= id_out_opcode;
+                ex_in_wb_enable <= id_out_wb_enable;
+                ex_in_store_enable <= id_out_store_enable;
+                ex_in_imm_enable  <= id_out_imm_enable;
+                ex_in_load_enable <= id_out_load_enable;
+                ex_in_rd <= id_out_rd;
+
+                --select the mux control signal
+                if (id_reg1_internal =ex_out_rd and id_reg2_internal= ex_out_rd) then
+                    -- both need excution stage result forwarding,just select the result of excution since it is the most recent data
+                    ex_control_rs_select_forwarding <= '0';
+                    ex_control_rs_select_forwarding_mem <='1';
+                    ex_control_rt_select_forwarding <= '0';
+                    ex_control_rt_select_forwarding_mem <= '1';                        
+                elsif (id_reg1_internal =ex_out_rd and id_reg2_internal /= ex_out_rd) then
+                    if (id_reg2_internal = mem_out_rd) then 
+                        -- rs is using the result from excution, but rt is using  the result from memory stage
+                        ex_control_rs_select_forwarding <= '0';
+                        ex_control_rs_select_forwarding_mem <='1';
+                        ex_control_rt_select_forwarding <= '1';
+                        ex_control_rt_select_forwarding_mem <= '1';
+                    else
+                        -- rs is using the result from excution, but rt does not use the forwarding signal
+                        ex_control_rs_select_forwarding <= '0';
+                        ex_control_rs_select_forwarding_mem <='1';
+                        ex_control_rt_select_forwarding <= '0';
+                        ex_control_rt_select_forwarding_mem <= '0';   
+                    end if;
+                elsif (id_reg1_internal /=ex_out_rd and id_reg2_internal = ex_out_rd) then 
+                    if (id_reg1_internal = mem_out_rd) then
+                        -- rs does not use result of ex, but use result of mem, rt use the result from ex
+                        ex_control_rs_select_forwarding <= '1';
+                        ex_control_rs_select_forwarding_mem <='1';
+                        ex_control_rt_select_forwarding <= '0';
+                        ex_control_rt_select_forwarding_mem <= '1';                            
+                    else
+                        -- rs does not use forwarding signal, rt use the result from ex
+                        ex_control_rs_select_forwarding <= '0';
+                        ex_control_rs_select_forwarding_mem <='0';
+                        ex_control_rt_select_forwarding <= '0';
+                        ex_control_rt_select_forwarding_mem <= '1';   
+                    end if;
+                elsif (id_reg1_internal /=ex_out_rd and id_reg2_internal /= ex_out_rd) then 
+                    if (id_reg1_internal = mem_out_rd and id_reg2_internal = mem_out_rd) then  
+                        -- rs does not use result of ex, but use result of mem, rt does not use result of ex, but use result of mem,
+                        ex_control_rs_select_forwarding <= '1';
+                        ex_control_rs_select_forwarding_mem <='1';
+                        ex_control_rt_select_forwarding <= '1';
+                        ex_control_rt_select_forwarding_mem <= '1';  
+                    elsif (id_reg1_internal = mem_out_rd and id_reg2_internal /= mem_out_rd) then  
+                        -- rs does not use result of ex, but use result of mem, rt does not use forward signal
+                        ex_control_rs_select_forwarding <= '1';
+                        ex_control_rs_select_forwarding_mem <='1';
+                        ex_control_rt_select_forwarding <= '0';
+                        ex_control_rt_select_forwarding_mem <= '0'; 
+                    elsif (id_reg1_internal /= mem_out_rd and id_reg2_internal = mem_out_rd) then  
+                        -- rs does not use forward signal, rt does not use result of ex, but use result of mem,
+                        ex_control_rs_select_forwarding <= '0';
+                        ex_control_rs_select_forwarding_mem <='0';
+                        ex_control_rt_select_forwarding <= '1';
+                        ex_control_rt_select_forwarding_mem <= '1'; 
+                    else
+                        -- both rs and rt do not use forward signal 
+                        ex_control_rs_select_forwarding <= '0';
+                        ex_control_rs_select_forwarding_mem <='0';
+                        ex_control_rt_select_forwarding <= '0';
+                        ex_control_rt_select_forwarding_mem <= '0';                             
+                    end if; 
+                end if;
+
+                -- EX to MEM
+                mem_in_data <= ex_out_mem_data;
+                mem_in_address <= ex_out_alu_data; 
+                mem_in_write_enable <= ex_out_store_enable;
+                mem_in_load_enable <= ex_out_load_enable;
+                mem_in_rd <= ex_out_rd;  
+                mem_in_wb_enable <= ex_out_wb_enable;
+
+                -- MEM to WB
+                wb_in_wb_enable <= mem_out_wb_enable;
+                wb_in_wb_rd <= mem_out_rd;
+                wb_in_wb_data <= mem_out_data;
+
+                -- WB to ID
+                wb_write_enable  <= wb_out_enable;
+                wb_write_address <= wb_out_rd;
+                wb_data  <= wb_out_data;    
+
+
+            end if;
 
         END IF;
     END PROCESS
+
+    -- allow to write back to register file
+    id_in_wb_write_enable  <= wb_out_enable;
+    id_in_wb_write_address <= wb_out_rd;
+    id_in_wb_data  <= wb_out_data;
 
 END ARCHITECTURE;
